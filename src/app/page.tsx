4d380065de408d44536f940772a992824905a8d1
Jonathan Lopez Ramos <jonathanlopezramos@MacBook-Air-de-Jonathan.local>
2024-03-22 02:54:43 -0600

    video 1.9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-
-import styles from "./page.module.css";
+import { Hero } from "app/components/home/Hero";
+import { Description } from "app/components/home/Description";
+import { MainProducts } from "app/components/home/MainProducts";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Holaaa mundo 123</h1>
+    <main>
+      <Hero/>
+      <Description/>
+      <MainProducts/>
     </main>
   );
 }
---
+++ src/components/home/MainProducts/MainProducs.tsx
@@ -0,0 +1,7 @@
+export const MainProducts = () => {
+    return(
+        <section>
+            <h1>MainProduts</h1>
+        </section>
+    )
+}
