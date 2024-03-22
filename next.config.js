5bdf4ef156e3d0e60e752a1772a2b3a95b722993
Jonathan Lopez Ramos <jonathanlopezramos@MacBook-Air-de-Jonathan.local>
2024-03-22 10:25:41 -0600

    video 3.4

--- next.config.js
+++ next.config.js
@@ -1,14 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 const path = require('path')
-
+ 
 const nextConfig = {
 
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/sass')],
 
-    prependData: `@import "variables.sass"`,
+    prependData: `@import "main.sass"`
   },
+  images: {
+    remotePatterns: [{
+      hostname: 'cdn.shopify.com',
+      
+      protocol: 'https',
+    }]
+  }
 
 };
 
