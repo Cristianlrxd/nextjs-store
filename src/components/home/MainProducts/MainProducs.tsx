6f055672a4290284e2f651b5abca678540a9fb7a
Jonathan Lopez Ramos <jonathanlopezramos@MacBook-Air-de-Jonathan.local>
2024-03-22 09:18:13 -0600

    Video 3.3

--- src/components/home/MainProducts/MainProducs.tsx
+++ src/components/home/MainProducts/MainProducs.tsx
@@ -1,7 +1,25 @@
-export const MainProducts = () => {
+
+const getProducts = async () => {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-01/products.json`,{
+        headers: new Headers ({
+            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || "",
+        })
+    })
+    const data =await response.json()
+    return data
+}
+
+export const MainProducts = async() => {
+
+    const products = await getProducts()
+    console.log(products)
+    //console.log("variable de entorno",process.env.NEXT_PUBLIC_SHOPIFY_HOSTNAME)
+
     return(
         <section>
-            <h1>MainProduts</h1>
+
+            <h1>Main Products</h1>
+
         </section>
     )
 }
