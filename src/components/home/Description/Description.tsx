b93164c55477b320a8ff38f163d43d8294de804e
Jonathan Lopez Ramos <jonathanlopezramos@MacBook-Air-de-Jonathan.local>
2024-03-22 04:59:07 -0600

    video 2.4

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,9 @@
 export const Description = () => {
     return(
         <section>
-            <h1>Description</h1>
+            <img src="/images/cuadrado.jpeg" alt="Products marketplace" />
+            <h2>Description</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum numquam dignissimos hic consequuntur nulla pariatur. Numquam eius voluptas vitae quidem at corporis, consequuntur non atque quaerat quasi quod dignissimos soluta.</p>
         </section>
     )
 }
