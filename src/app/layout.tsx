fbb413e026215719664d8c64328c905982c71a84
Jonathan Lopez Ramos <jonathanlopezramos@MacBook-Air-de-Jonathan.local>
2024-03-22 11:39:23 -0600

    video 4.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import { Roboto } from 'next/font/google'
 import { Header } from 'app/components/shared/Header'
 import { Footer } from "app/components/shared/Footer";
-import { Hero } from "app/components/home/Hero"
-import { Description } from "app/components/home/Description"
 import 'app/sass/globals.sass'
 
 const roboto = Roboto({
--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -7,6 +7,9 @@ interface CategoryProps{
 
 export default function Category (props: CategoryProps) {
     const {categories} = props.params 
+    //throw new Error('Error: Boom!')
+     // Throw Error #2
+     // console.log(categories)
     return(
         <h1> Categoria dinamica: {categories} </h1>
     )
