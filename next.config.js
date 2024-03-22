233ad8e6c2035c7d47994eddc4cc1f1581719207
Jonathan Lopez Ramos <jonathanlopezramos@MacBook-Air-de-Jonathan.local>
2024-03-22 04:21:55 -0600

    video 2.2

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+
+const path = require('path')
+
+const nextConfig = {
+
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/sass')],
+
+    prependData: `@import "variables.sass"`,
+  },
+
+};
+
+module.exports = nextConfig
