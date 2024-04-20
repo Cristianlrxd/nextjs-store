c8ede16b01105f78287f59807cc0d391085518bf
Cristianlrxd <lopezramoscristian6@gmail.com>
2024-04-20 08:54:05 -0600

    video 4.8

--- src/app/store/[...category]/leyout.tsx
+++ src/app/store/[...category]/leyout.tsx
@@ -1,11 +1,24 @@
-
-export default function Leyout({ children }: { children: React.ReactNode}) {
-    return(
-        <main>
-            <nav>Navegacion de las categorias</nav>
-
-            {children}
-            
+import { getCollections } from "app/services/shopify/collections"
+import styles from "./StoreLayout.module.sass"
+import Link from "next/link"
+export default async function Layout({children}:{children: React.ReactNode }){
+    const collections =  await getCollections()
+    return (
+        <main className={styles.StoreLayout}>
+          <h1>Explore</h1>
+          <nav>
+            <ul className={styles.StoreLayout__list}>
+              {
+                collections?.map((collection: any) => (
+                  <Link key={collection.id} href={'/store/' + collection.handle} className={styles.StoreLayout__chip}>
+                    {collection.title}
+                  </Link>
+                ))
+              }
+            </ul>
+          </nav>
+          {children}
         </main>
-    )
+      )
+
 }
